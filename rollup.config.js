451e81214f5960868255fd60e65b0f7144909fa4
Chase Williams <chasewilliams98@gmail.com>
2022-12-17 21:45:50 -0500

    added scss

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import scss from "rollup-plugin-scss";
 
 const packageJson = require("./package.json");
 
@@ -24,11 +25,13 @@ export default [
 			resolve(),
 			commonjs(),
 			typescript({ tsconfig: "./tsconfig.json" }),
+			scss(),
 		],
 	},
 	{
 		input: "dist/esm/index.d.ts",
 		output: [{ file: "dist/index.d.ts", format: "esm" }],
 		plugins: [dts()],
+		external: [/\.scss$/],
 	},
 ];
